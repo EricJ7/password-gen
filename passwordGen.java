08179de93b84dbd5e42e6ba6633afbc7009fef94
EricJ7 <67254893+EricJ7@users.noreply.github.com>
2021-12-27 16:45:18 -0500

    Update passwordGen.java

--- passwordGen.java
+++ passwordGen.java
@@ -1,35 +1,32 @@
 import java.util.Scanner;
 import java.util.Random;
 
-
-//password generator
 public class passwordGen {
 
 	
-		//instance of random class
 		Random rand = new Random(); 
 			
 		//random int from 0 - 52 (characters)
-		private int randInt = rand.nextInt(52);
+		//private int randInt = rand.nextInt(52);
 			
 		//random int from 0 - 7 (symbols)
-		private int randIntSym = rand.nextInt(7);
+		//private int randIntSym = rand.nextInt(7);
 			
 		//random int from 0 - 8 (numbers)
-		private int randIntNum = rand.nextInt(8);
+		//private int randIntNum = rand.nextInt(8);
 			
 	
 	public static void main(String[]args) {
 		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the length of the password");
+		System.out.println("Enter the length of the password: ");
 		int passLen  = input.nextInt();
 		System.out.println(passGeneration(passLen));
 		
 	}//main
 	
 	//returns password of type char
-	static char[] passGeneration(int pLen) {
+	static char[] passGeneration(int passLen) {
 		
 		//every possible number value in base 10, every character, etc
 		System.out.println(pLen + " character password: ");
@@ -43,12 +40,13 @@ public class passwordGen {
 		Random rand = new Random();
 		
 		//creates array of characters (passwords), with length specified by user 
-		char[] password = new char[pLen];
-		for(int index = 0; index < pLen; index++) {
+		char[] password = new char[passLen];
+		for(int index = 0; index < passLen; index++) {
+			// adds random characters from passSymbols 
 			password[index] = passSymbols.charAt(rand.nextInt(passSymbols.length()));
 		}
 		
 		return password;
 
-	}//passgeneration
-}//passWgen
+	}
+}
