77de8d6b059e17c79b1072d97521ad0c44b10c31
EricJ7 <67254893+EricJ7@users.noreply.github.com>
2021-12-27 16:51:41 -0500

    Update passwordGen.java

--- passwordGen.java
+++ passwordGen.java
@@ -3,33 +3,21 @@ import java.util.Random;
 
 public class passwordGen {
 
-	
 		Random rand = new Random(); 
-			
-		//random int from 0 - 52 (characters)
-		//private int randInt = rand.nextInt(52);
-			
-		//random int from 0 - 7 (symbols)
-		//private int randIntSym = rand.nextInt(7);
-			
-		//random int from 0 - 8 (numbers)
-		//private int randIntNum = rand.nextInt(8);
-			
-	
+
 	public static void main(String[]args) {
 		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the length of the password: ");
-		int passLen  = input.nextInt();
+		int passLen = input.nextInt();
 		System.out.println(passGeneration(passLen));
 		
 	}//main
 	
-	//returns password of type char
 	static char[] passGeneration(int passLen) {
 		
 		//every possible number value in base 10, every character, etc
-		System.out.println(pLen + " character password: ");
+		System.out.println(passLen + " character password: ");
 		String characters = "abcdefghijklmnopqrstuvwxyz";
 		String charsCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String numbers = "123456790";
